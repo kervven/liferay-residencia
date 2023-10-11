9f46124f3c591e43acbae08eb51f1981f13d0c1a
Kerven Kildhery <kervensilvva?@gmail.com>
2023-10-11 15:03:25 -0300

    projeto criado

---
+++ src/main/java/com/liferay/residencialiferay/ResidencialiferayApplication.java
@@ -0,0 +1,13 @@
+package com.liferay.residencialiferay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidencialiferayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidencialiferayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liferay/residencialiferay/ResidencialiferayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liferay.residencialiferay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidencialiferayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
